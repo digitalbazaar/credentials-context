88dc2f29b44d28ca5e2332682496e40605d7005c
kimdhamilton <kimdhamilton@gmail.com>
2021-05-07 11:03:46 -0700

    nofs

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,13 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
 'use strict';
 
-exports.CREDENTIALS_CONTEXT_V1_URL = 'https://www.w3.org/2018/credentials/v1';
+module.exports = {
+  CONTEXT_FILENAME: 'credentials-v1.jsonld',
+  CONTEXT_URL: 'https://www.w3.org/2018/credentials/v1'
+};
+
+
+
+
