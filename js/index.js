5d3dc86cce19621cb059f5af2037848de1fb925d
David I. Lehn <dlehn@digitalbazaar.com>
2023-11-29 23:47:33 -0500

    Add draft v2 context.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
 /*!
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
-const context = require('./context');
+const contextV1 = require('./context-v1');
+const contextV2 = require('./context-v2');
 const constants = require('./constants');
 const {documentLoader} = require('./documentLoader');
-const {CONTEXT_URL} = constants;
+const {
+  CREDENTIALS_CONTEXT_V1_URL,
+  CREDENTIALS_CONTEXT_V2_URL,
+  CONTEXT_URL
+} = constants;
 
 const contexts = new Map();
-contexts.set(constants.CONTEXT_URL, context);
+contexts.set(constants.CREDENTIALS_CONTEXT_V1_URL, contextV1);
+contexts.set(constants.CREDENTIALS_CONTEXT_V1_URL, contextV2);
 
 module.exports = {
   constants,
   contexts,
   documentLoader,
+  CREDENTIALS_CONTEXT_V1_URL,
+  CREDENTIALS_CONTEXT_V2_URL,
+  CONTEXT_URL_V1: CREDENTIALS_CONTEXT_V1_URL,
+  CONTEXT_URL_V2: CREDENTIALS_CONTEXT_V2_URL,
   CONTEXT_URL,
-  CREDENTIALS_CONTEXT_V1_URL: CONTEXT_URL,
-  CONTEXT_URL_V1: CONTEXT_URL,
-  CONTEXT: context
+  CONTEXT_V1: contextV1,
+  CONTEXT_V2: contextV2,
+  CONTEXT: contextV1
 };
